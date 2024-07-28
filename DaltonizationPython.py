b3a30bc809a11147b03a5920ec14d36582580ead
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2024-07-29 05:20:03 +0800

    Updated base algorithm for video and image
    Added sample colorblind image
    Added Image version
    Streamlined non-CUDA algorithms

--- DaltonizationPython.py
+++ DaltonizationPython.py
@@ -1,73 +1,81 @@
 import cv2 as cv
 import numpy as np
 
-
-def daltonize(image, level, deficiency_type):
+def daltonize(img, intensity, deficiency):
     
-    RGB_to_LMS = np.array([[17.8824,43.5161,4.11935],[3.45565,27.1554,3.86714],[0.0299566,0.184309,1.46709]])
-
+    # Image RGB
+    RGB = np.asarray(img, dtype=float)
+    
+    # Colorspace matrices
+    RGB_to_LMS = np.array([[17.8824,43.5161,4.11935],
+                           [3.45565,27.1554,3.86714],
+                           [0.0299566,0.184309,1.46709]])
     LMS_to_RGB = np.linalg.inv(RGB_to_LMS)
-
+    
+    # Deficiency matrices
     daltonization_matrices = {
-        "Deuteranomaly": np.array([[1, 0, 0],
-                                    [0.4942, 0, 1.2483],
-                                    [0, 0, 1]]),
-        "Protanomaly": np.array([[0, 2.0234, -2.5258],
-                                  [0, 1, 0],
-                                  [0, 0, 1]]),
-        "Tritanomaly": np.array([[1, 0, 0],
-                                  [0, 1, 0],
-                                  [-0.395913, 0.801109, 0]])
+        'd': np.array([ [1, 0, 0],
+                        [0.4942, 0, 1.2483],
+                        [0, 0, 1]]),
+        'p': np.array([ [0, 2.0234, -2.5258],
+                        [0, 1, 0],
+                        [0, 0, 1]]),
+        't': np.array([ [1,0,0],
+                        [0,1,0],
+                        [-0.395913,0.801109,0]])
     }
 
-    daltonization_matrix = daltonization_matrices.get(deficiency_type, None)
-
+    daltonization_matrix = daltonization_matrices.get(deficiency, None)
     if daltonization_matrix is None:
-        raise ValueError("Invalid deficiency type. Supported types are: Deuteranomaly, Protanomaly, Tritanomaly")
-
-
-    image_rgb = image / 255.0
+        raise ValueError("Invalid deficiency type. Supported types are: Deuteranomaly(d), Protanomaly(p), Tritanomaly(t)")
     
-    # 1. Convert RGB to LMS
-    image_lms = np.dot(image_rgb, RGB_to_LMS.T)
+    # Shift matrix
+    shift = np.array([[0,0,0],
+                      [0.7,1,0],
+                      [0.7,0,1]])
     
-    # 2. Simulate color blindness
-    simulated_lms = np.dot(image_lms, daltonization_matrix.T)
+    # RGB to LMS
+    LMS = np.tensordot(RGB[...,:3], RGB_to_LMS.T, axes=1)
     
-    # 3. Apply the daltonization correction
-    corrected_lms = image_lms + level * (image_lms - simulated_lms)
-    
-    # 4. Convert LMS back to RGB
-    corrected_rgb = np.dot(corrected_lms, LMS_to_RGB.T)
-    corrected_rgb = np.clip(corrected_rgb, 0, 1)
-    corrected_image = (corrected_rgb * 255).astype(np.uint8)
-    
-    return corrected_image
+    # LMS to simul LMS
+    _LMS = np.tensordot(LMS[...,:3], daltonization_matrix.T, axes=1)
 
+    # simul LMS to simul RGB
+    _RGB = np.tensordot(_LMS[...,:3], LMS_to_RGB.T, axes=1)
+    
+    # simul RGB to compensated RGB
+    error = (RGB - _RGB) * intensity
+    ERR = np.tensordot(error[...,:3], shift.T, axes=1)
+    cRGB = ERR + RGB
 
+    # Clip the values to be within [0, 255]
+    result = np.clip(cRGB, 0, 255).astype('uint8')
+    
+    return result
 
+# Default parameters
 correction_level = 100
-correction_types = ["Deuteranomaly", "Protanomaly", "Tritanomaly"]
-current_correction_type = "Deuteranomaly"
+correction_types = ["d", "p", "t"]
+default_correction_type = "d"
 
+# Input video
+vid = cv.VideoCapture(0)
+
+# GUI
 window_name = 'Daltonization for Color Deficiency Correction'
 cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
-
 cv.createTrackbar("Correction Level (%)", window_name, correction_level, 500, lambda x: x)
 cv.createTrackbar("Correction Type", window_name, 0, len(correction_types) - 1, lambda x: x)
 
-
-vid = cv.VideoCapture(0)
-
 while True:
     ret, frame = vid.read()
-
+    
     level = cv.getTrackbarPos("Correction Level (%)", window_name) / 100.0
     correction_type_index = cv.getTrackbarPos("Correction Type", window_name)
     
-    current_correction_type = correction_types[correction_type_index]
+    default_correction_type = correction_types[correction_type_index]
 
-    corrected = daltonize(frame, level, current_correction_type)
+    corrected = daltonize(frame, level, default_correction_type)
 
     cv.putText(frame, "Original", (10,30),
                cv.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 2)
---
+++ DaltonizationPythonIMG.py
@@ -0,0 +1,104 @@
+import cv2 as cv
+import numpy as np
+
+def daltonize(img, intensity, deficiency):
+    
+    # Image RGB
+    RGB = np.asarray(img, dtype=float)
+    
+    # Colorspace matrices
+    RGB_to_LMS = np.array([[17.8824,43.5161,4.11935],
+                           [3.45565,27.1554,3.86714],
+                           [0.0299566,0.184309,1.46709]])
+    LMS_to_RGB = np.linalg.inv(RGB_to_LMS)
+    
+    # Deficiency matrices
+    daltonization_matrices = {
+        'd': np.array([ [1, 0, 0],
+                        [0.4942, 0, 1.2483],
+                        [0, 0, 1]]),
+        'p': np.array([ [0, 2.0234, -2.5258],
+                        [0, 1, 0],
+                        [0, 0, 1]]),
+        't': np.array([ [1,0,0],
+                        [0,1,0],
+                        [-0.395913,0.801109,0]])
+    }
+
+    daltonization_matrix = daltonization_matrices.get(deficiency, None)
+    if daltonization_matrix is None:
+        raise ValueError("Invalid deficiency type. Supported types are: Deuteranomaly(d), Protanomaly(p), Tritanomaly(t)")
+    
+    # Shift matrix
+    shift = np.array([[0,0,0],
+                      [0.7,1,0],
+                      [0.7,0,1]])
+    
+    # RGB to LMS
+    LMS = np.tensordot(RGB[...,:3], RGB_to_LMS.T, axes=1)
+    
+    # LMS to simul LMS
+    _LMS = np.tensordot(LMS[...,:3], daltonization_matrix.T, axes=1)
+
+    # simul LMS to simul RGB
+    _RGB = np.tensordot(_LMS[...,:3], LMS_to_RGB.T, axes=1)
+    
+    # simul RGB to compensated RGB
+    error = (RGB - _RGB) * intensity
+    ERR = np.tensordot(error[...,:3], shift.T, axes=1)
+    cRGB = ERR + RGB
+
+    # Clip the values to be within [0, 255]
+    result = np.clip(cRGB, 0, 255).astype('uint8')
+    
+    return result
+
+# Default parameters
+correction_level = 100
+correction_types = ["d", "p", "t"]
+default_correction_type = "d"
+
+# Get image data
+path = "Images/colorblind.jpg" # Should work, if it doesn't, use full path to img
+frame = cv.imread(path)
+
+# GUI
+window_name = 'Daltonization for Color Deficiency Correction'
+cv.namedWindow(window_name, cv.WINDOW_AUTOSIZE)
+cv.createTrackbar("Correction Level (%)", window_name, correction_level, 500, lambda x: x)
+cv.createTrackbar("Correction Type", window_name, 0, len(correction_types) - 1, lambda x: x)
+
+while True:
+    
+    # Get window size
+    window_width, window_height = cv.getWindowImageRect(window_name)[2:4]
+    
+    # Calculate new size to maintain aspect ratio
+    height, width = frame.shape[:2]
+    aspect_ratio = width / height
+    if window_width / aspect_ratio <= window_height:
+        new_width = window_width
+        new_height = int(window_width / aspect_ratio)
+    else:
+        new_height = window_height
+        new_width = int(window_height * aspect_ratio)
+
+    resized_frame = cv.resize(frame, (new_width, new_height))
+
+    level = cv.getTrackbarPos("Correction Level (%)", window_name) / 100.0
+    correction_type_index = cv.getTrackbarPos("Correction Type", window_name)
+    
+    default_correction_type = correction_types[correction_type_index]
+
+    corrected = daltonize(resized_frame, level, default_correction_type)
+
+    cv.putText(resized_frame, "Original", (10,30),
+               cv.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 2)
+
+    cv.imshow(window_name, np.hstack([resized_frame, corrected]))
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+frame.release()
+cv.destroyAllWindows()
