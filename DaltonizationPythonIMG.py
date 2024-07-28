1d35a8c6fd142f0ed67cbaf5b08065d2fb5b6b2b
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2024-07-29 07:51:43 +0800

    Corrected Base Algorithm

--- DaltonizationPythonIMG.py
+++ DaltonizationPythonIMG.py
@@ -1,16 +1,22 @@
 import cv2 as cv
 import numpy as np
 
+# Colorspace matrices
+RGB_to_LMS = np.array([[17.8824,43.5161,4.11935],
+                        [3.45565,27.1554,3.86714],
+                        [0.0299566,0.184309,1.46709]])
+LMS_to_RGB = np.array([[0.0809,-0.1305,0.11672],
+                        [-0.01025,0.054019327,-0.11361],
+                        [-0.0003653,-0.004122,0.69351]]) # Inverse of RGB_to_LMS
+# Shift matrix
+shift = np.array([[0,0,0],
+                    [0.7,1,0],
+                    [0.7,0,1]])
+
 def daltonize(img, intensity, deficiency):
     
     # Image RGB
     RGB = np.asarray(img, dtype=float)
-    
-    # Colorspace matrices
-    RGB_to_LMS = np.array([[17.8824,43.5161,4.11935],
-                           [3.45565,27.1554,3.86714],
-                           [0.0299566,0.184309,1.46709]])
-    LMS_to_RGB = np.linalg.inv(RGB_to_LMS)
     
     # Deficiency matrices
     daltonization_matrices = {
@@ -29,24 +35,19 @@ def daltonize(img, intensity, deficiency):
     if daltonization_matrix is None:
         raise ValueError("Invalid deficiency type. Supported types are: Deuteranomaly(d), Protanomaly(p), Tritanomaly(t)")
     
-    # Shift matrix
-    shift = np.array([[0,0,0],
-                      [0.7,1,0],
-                      [0.7,0,1]])
-    
     # RGB to LMS
     LMS = np.tensordot(RGB[...,:3], RGB_to_LMS.T, axes=1)
     
     # LMS to simul LMS
     _LMS = np.tensordot(LMS[...,:3], daltonization_matrix.T, axes=1)
 
-    # simul LMS to simul RGB
-    _RGB = np.tensordot(_LMS[...,:3], LMS_to_RGB.T, axes=1)
+    # simul LMS to compensated LMS
+    error = (LMS - _LMS) * intensity
+    cLMS = np.tensordot(error[...,:3], shift.T, axes=1)
     
-    # simul RGB to compensated RGB
-    error = (RGB - _RGB) * intensity
-    ERR = np.tensordot(error[...,:3], shift.T, axes=1)
-    cRGB = ERR + RGB
+    # compensated LMS to compensated RGB
+    _RGB = np.tensordot(cLMS[...,:3], LMS_to_RGB.T, axes=1)
+    cRGB = _RGB + RGB
 
     # Clip the values to be within [0, 255]
     result = np.clip(cRGB, 0, 255).astype('uint8')
@@ -100,5 +101,5 @@ while True:
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
-frame.release()
+im.release()
 cv.destroyAllWindows()
