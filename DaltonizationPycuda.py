ff621d8e1903a30ecf4602095672a0bd84d9cea0
smtLasalle <shawne_tumalad@dlsu.edu.ph>
2024-07-30 12:57:53 +0800

    Quick hotfix
    added device synchronizing

--- DaltonizationPycuda.py
+++ DaltonizationPycuda.py
@@ -114,6 +114,7 @@ def daltonize_gpu(img, intensity, deficiency):
     grid = (int(np.ceil(width / block[0])), int(np.ceil(height / block[1])), 1)
     
     daltonize_kernel(img_gpu, np.float32(intensity), np.int8(ord(deficiency)), result_gpu, np.int32(width), np.int32(height), block=block, grid=grid)
+    cuda.Context.synchronize()
     
     cuda.memcpy_dtoh(result, result_gpu)
     result = result.reshape((height, width, channels)).astype(np.uint8)
